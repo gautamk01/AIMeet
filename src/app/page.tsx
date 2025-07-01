ec15502c815a42bf6d175591a9756d5f369bd204
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-07-01 22:04:44 +0530

    Just completed 03:auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,97 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //session
+  const { data: session } = authClient.useSession();
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+  const onLogin = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.email}</p>
+        <Button variant="destructive" onClick={() => authClient.signOut()}>
+          {" "}
+          Sign out
+        </Button>
+      </div>
+    );
+  }
   return (
-    <div className="">
-      Hello World
-      <Button variant="ghost">click me</Button>
+    <div>
+      <h1>Create user</h1>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onSubmit}>Create user </Button>
+      </div>
+      <h1>Login</h1>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={onLogin}>Login </Button>
+      </div>
     </div>
   );
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 const db = drizzle(process.env.DATABASE_URL!);
 
-
+export default db
