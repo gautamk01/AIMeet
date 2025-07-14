b1dc5dac4628632d4652bb02f358870bdbaab958
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-07-14 21:36:48 +0530

    Worked on  Dashboard Navbar 1. Created the Navbar 2.Add Responsive Drawer to Dashboard user Button 3. Nav bar search button with shortcut

--- src/modules/Dashboard/ui/comonents/dashboard-user-button.tsx
+++ src/modules/Dashboard/ui/comonents/dashboard-user-button.tsx
@@ -14,10 +14,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/custom_ui/generated-avatar";
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
   const { data, isPending } = authClient.useSession();
   const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const isMobil = useIsMobile();
+
   const router = useRouter();
   const onlogout = () => {
     setIsLoggingOut(true);
@@ -36,11 +48,51 @@ export const DashboardUserButton = () => {
   if (isPending || !data?.user) {
     return null;
   }
+  if (isMobil) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+          {data.user.image ? (
+            <Avatar>
+              <AvatarImage src={data.user.image}></AvatarImage>
+            </Avatar>
+          ) : (
+            <GeneratedAvatar
+              seed={data.user.name}
+              variant="initials"
+              className="size-9 mr-3"
+            />
+          )}
+          <div className=" flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+            <p className="text-sm truncate w-full">{data.user.name}</p>
+            <p className=" text-xs truncate w-full">{data.user.email}</p>
+          </div>
+          <ChevronDownIcon className=" size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerTitle>{data.user.email}</DrawerTitle>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant="outline" onClick={() => {}}>
+              <CreditCardIcon className=" size-4 text-black" />
+              Billing
+            </Button>
+            <Button variant="outline" onClick={onlogout}>
+              <LogOutIcon className=" size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger // Attach the ref to the element
-        className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden"
+        className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2"
       >
         {data.user.image ? (
           <Avatar>
