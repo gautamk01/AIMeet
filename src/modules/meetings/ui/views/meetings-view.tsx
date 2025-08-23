c18a6fd8251f81596b13d6ede903f1a0e2f1853d
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-08-23 19:26:39 +0530

    - created the meeting routes - created the meeting routes in app dir and dynamic routes also created - render a test data fectch from  the db - completed the setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { ErrorState } from "@/components/custom_ui/error-state";
+import { LoadingState } from "@/components/custom_ui/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+const MeetingsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export const MeetingViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      description="Something went wrong"
+    />
+  );
+};
+
+export default MeetingsView;
