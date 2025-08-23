ebda4835a2d6c048a46fb44b239cb0cef581a9dd
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-08-23 15:22:54 +0530

    Completed the agent update and delete procedure and schema

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -1,7 +1,7 @@
 import db from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schema";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schema";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
@@ -9,6 +9,26 @@ import { TRPCError } from "@trpc/server";
 
 
 export const agentRouter = createTRPCRouter({
+
+    update: protectedProcedure.input(agentsUpdateSchema).mutation(async ({ ctx, input }) => {
+        const [updateAgent] = await db.update(agents).set(input).where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))).returning();
+        if (!updateAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Update Agent not found" })
+        }
+
+        return updateAgent;
+    }),
+    remove: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
+        const [removedAgent] = await db.delete(agents).where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))).returning();
+
+        if (!removedAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+        }
+
+        return removedAgent;
+    }),
+
+
 
     //from this we are getting the inital values the update form will be populated with 
     getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
