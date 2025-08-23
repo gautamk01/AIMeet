be2c8245b9df5f46ec61ea29bb6eb4ed103e3567
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-08-23 11:55:37 +0530

    The Redirect the column is completed and working on the loading and error component also setup in agent id view

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,3 +1,5 @@
+import { ErrorState } from "@/components/custom_ui/error-state";
+import { LoadingState } from "@/components/custom_ui/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { use } from "react";
@@ -16,5 +18,23 @@ export const AgentIdView = ({ agentId }: Props) => {
     <div className=" flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
       {JSON.stringify(data, null, 2)}
     </div>
+  );
+};
+
+export const AgentIdViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+export const AgentIdViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Something went wrong"
+    />
   );
 };
