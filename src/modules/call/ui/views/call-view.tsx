4b7f7b2f218019c0544125de3c1456d3237a48d0
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-09-01 19:44:14 +0530

    - Add call page - Add call view - created full functional google meet experiance using stream including call lobby, call active and ca;ll ended page

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ErrorState } from "@/components/custom_ui/error-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { CallProvider } from "../components/call-provider";
 
 interface Props {
   meetingId: string;
@@ -12,5 +14,16 @@ export const CallView = ({ meetingId }: Props) => {
   const { data } = useSuspenseQuery(
     trpc.meetings.getOne.queryOptions({ id: meetingId })
   );
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+
+  if (data.status === "completed") {
+    return (
+      <div className=" flex h-screen items-center justify-center">
+        <ErrorState
+          title="Meeting has ended"
+          description="you can no longer join this meeting"
+        />
+      </div>
+    );
+  }
+  return <CallProvider meetingId={meetingId} meetingName={data.name} />;
 };
